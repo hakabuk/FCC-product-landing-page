ae2f6b92a68b3cec2eab4b0d2ba10e3ba1bd8b48
michal <michal@dataquest.io>
2018-06-20 10:20:30 +0100

    Fixed: logo was getting navbar active class

--- js/script.js
+++ js/script.js
@@ -2,10 +2,12 @@ $('.nav')
   .find('a')
   .click(function(e) {
     e.preventDefault();
-    $('a').removeClass('active');
-    $(this).addClass('active');
     var section = $(this).attr('href');
     $('html, body').animate({
       scrollTop: $(section).offset().top
     });
+    $('a').removeClass('active');
+    $(this).addClass('active');
+    // prevents logo from getting active class
+    $('#logo a').removeClass('active');
   });
